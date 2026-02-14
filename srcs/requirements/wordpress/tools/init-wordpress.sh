093995571d660a1a02f4d9b2b96c931b69cbb25a
VYAntares <endroahm@hotmail.com>
2026-02-14 01:18:26 +0000

    wordpress ok

---
+++ srcs/requirements/wordpress/tools/init-wordpress.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+while ! mysqladmin ping -h"mariadb" --silent ; do
+	echo "Waiting for MariaDB.."
+	sleep 2
+done
+echo "MariaDB is ready!"
+
+source /run/secrets/credentials
+
+if [ -f "/var/www/html/wp-config.php" ]; then
+	echo "Wordpress already installed, skipping.."
+else
+	echo "First installation.."
+	
+	# Télécharge WordPress dans /var/www/html
+	wp core download --allow-root
+
+	# Crée wp-config.php avec les informations de connexion à MariaDB
+	# Ce fichier indique à WordPress comment se connecter à la base de données
+	wp config create \
+		--dbname=${DB_NAME} \
+		--dbuser=${DB_USER} \
+		--dbpass=$(cat /run/secrets/db_password) \
+		--dbhost=${MYSQL_HOST} \
+		--allow-root
+
+	# Installe WordPress et crée l'utilisateur admin
+	wp core install \
+		--url=https://${DOMAIN_NAME} \
+		--title="Inception." \
+		--admin_user=admin \
+		--admin_password=${wp_admin_password} \
+		--admin_email=admin@example.com \
+		--allow-root
+
+	# Crée un deuxième utilisateur (non-admin)
+	wp user create \
+		user \
+		user@example.com \
+		--user_pass=${wp_user_password} \
+		--role=author \
+		--allow-root
+
+	chown -R www-data:www-data /var/www/html
+
+fi
+
+exec php-fpm7.4 -F
+
